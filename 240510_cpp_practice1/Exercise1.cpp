fea9ec4cf4509f65f2a75918b5c47c169895f4a0
dlskrud815 <dlskrud815@gmail.com>
2024-05-10 15:43:16 +0900

    Add project files.

---
+++ 240510_cpp_practice1/Exercise1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Rectangle
+{
+public:
+	Rectangle() {}
+
+	Rectangle(float width, float height)
+	{
+		m_width = width;
+		m_height = height;
+	}
+
+	float Area(float m_width, float m_height);
+
+private:
+	float m_width = 0;
+	float m_height = 0;
+};
+
+float Rectangle::Area(float m_width, float m_height)
+{
+	return m_width * m_height;
+}
+
+int main()
+{
+	float width, height;
+
+	cout << "사각형의 가로와 세로 길이를 입력해주세요. (띄어쓰기로 구분) ";
+	cin >> width >> height;
+	Rectangle rec{ width, height };
+
+	cout << "넓이는: " << rec.Area(width, height);
+
+	return 0;
+}
