8aa4afb6a9a9cedbb781133de78f449616d82551
dlskrud815 <dlskrud815@gmail.com>
2024-05-10 16:04:00 +0900

    Exercise1

--- 240510_cpp_practice1/Exercise1.cpp
+++ 240510_cpp_practice1/Exercise1.cpp
@@ -15,13 +15,6 @@ public:
 
 	float Area();
 
-	/*
-	float Area()
-	{
-		return m_width * m_height;
-	}
-	*/
-
 private:
 	float m_width = 0;
 	float m_height = 0;
@@ -39,7 +32,6 @@ int main()
 	cout << "사각형의 가로와 세로 길이를 입력해주세요. (띄어쓰기로 구분) ";
 	cin >> width >> height;
 	Rectangle rec{ width, height };
-	//Rectangle rec;
 
 	cout << "넓이는: " << rec.Area();
 
